63307ba0847721fa0d7aecd949c25c01c62e3bc7
Joonas Häkkinen <joonas@joonashakkinen.fi>
2018-09-09 15:32:50 +0300

    5.6 done

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,10 +1,48 @@
 import React from 'react';
 
 
-export default ({ blog }) => (
-  <div>
-    {
-      `${blog.title} ${blog.author}`
-    }
-  </div>
-);
+export default class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expanded: false,
+    };
+  }
+
+  toggleDetails = () => this.setState({ expanded: !this.state.expanded });
+
+
+  render() {
+    const { expanded } = this.state;
+    const { title, author,likes, url, user } = this.props.blog;
+
+    const blogStyle = {
+      padding: '10 0',
+    };
+
+    return (
+      <div style={blogStyle}>
+        <div onClick={this.toggleDetails}>
+          {`${title} / ${author}`}
+        </div>
+        <div style={{ display: expanded ? '' : 'none' }}>
+          <div>
+            <a href={url}>
+              {url}
+            </a>
+          </div>
+          <div>
+            {`${likes} likes`}
+            <button>
+              Like
+            </button>
+          </div>
+          <div>
+            {`Added by ${user.name}`}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
