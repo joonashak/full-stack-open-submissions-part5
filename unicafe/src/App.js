79e9512b1b589531af0bf0cf8452ba8224cb63c2
Joonas Häkkinen <joonas@joonashakkinen.fi>
2018-12-13 22:28:28 +0200

    5.17-18 done

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import reducer from './reducer';
+
+const store = createStore(reducer);
 
 class App extends Component {
   render() {
+    const { good, ok, bad } = store.getState();
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h4>
+          Anna palautetta
+        </h4>
+        <button onClick={() => store.dispatch({ type: 'GOOD' })}>
+          Hyvä
+        </button>
+        <button onClick={() => store.dispatch({ type: 'OK' })}>
+          Neutraali
+        </button>
+        <button onClick={() => store.dispatch({ type: 'BAD' })}>
+          Huono
+        </button>
+
+        <h4>
+          Statistiikka
+        </h4>
+        <p>
+          {`hyvä ${good}`}
+        </p>
+        <p>
+          {`neutraali ${ok}`}
+        </p>
+        <p>
+          {`huono ${bad}`}
+        </p>
+        <p>
+          {`hyviä ${Math.round(good / (good + ok + bad) * 100) || '-'} %`}
+        </p>
+        <button onClick={() => store.dispatch({ type: 'ZERO' })}>
+          nollaa tilasto
+        </button>
       </div>
     );
   }
 }
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+renderApp();
+store.subscribe(renderApp);
 
 export default App;
